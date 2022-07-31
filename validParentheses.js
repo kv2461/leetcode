c9cc64a39c1862dcce95ffe586efff361d64bdd5
Kevin Vicencio <kevinarnoldcode@gmail.com>
2022-07-31 15:25:22 -0700

    added validParentheses

---
+++ validParentheses.js
@@ -0,0 +1,43 @@
+// 20. Valid Parentheses
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+var isValid = (s) => {
+    const bracketMap = new Map([
+        [')', '('],
+        ['}', '{'],
+        [']', '['],
+      ]);
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (bracketMap.has(s[i])) {
+            const pop = stack.pop();
+            if (pop !== bracketMap.get(s[i])) {
+                return false;
+            }
+        } else {
+            stack.push(s[i]);
+        }
+    }
+
+    return stack.length === 0;
+}
